87a3670a34f74b5911f0fd8ec1c8d01543454971
dinhnam72 <dinhnam20162011@gmail.com>
2023-09-14 16:26:28 +0700

    nop bai tap case study

--- case_study/furama.sql
+++ case_study/furama.sql
@@ -211,7 +211,7 @@ VALUES
 -- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
 SELECT * 
 FROM nhan_vien
-WHERE LENGTH(ho_ten) <=15 
+WHERE CHAR_LENGTH(ho_ten) <=15 
 AND (SUBString_INDEX(ho_ten,' ',-1) LIKE 'H%' 
 OR SUBString_INDEX(ho_ten,' ',-1) LIKE 'T%' 
 OR SUBString_INDEX(ho_ten,' ',-1) LIKE 'K%');
@@ -229,84 +229,108 @@ OR SUBString_INDEX(ho_ten,' ',-1) LIKE 'K%');
     ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang 
     JOIN loai_khach
     ON khach_hang.ma_loai_khach= loai_khach.ma_loai_khach
-    WHERE khach_hang.ma_loai_khach=1
+    WHERE loai_khach.ten_loai_khach='Diamond'
     GROUP BY khach_hang.ma_khach_hang
     ORDER BY COUNT(khach_hang.ma_khach_hang);
    
 -- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien (Với tổng tiền được tính theo 
 -- công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
 -- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
-	SELECT    
-     kh.ma_khach_hang, 
-     kh.ho_ten,
-     lk.ten_loai_khach, 
-     hd.ma_hop_dong, 
-     dv.ten_dich_vu,
-     hd.ngay_lam_hop_dong,
-     hd.ngay_ket_thuc,
-	 ((dv.chi_phi_thue) + (hdct.so_luong * dvdk.gia)) AS tong_tien
-     FROM khach_hang AS kh
-     LEFT JOIN loai_khach AS lk ON kh.ma_loai_khach = lk.ma_loai_khach
-     LEFT JOIN hop_dong AS hd ON kh.ma_khach_hang = hd.ma_khach_hang
-     LEFT JOIN hop_dong_chi_tiet AS hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
-     LEFT JOIN dich_vu_di_kem AS dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
-     LEFT JOIN dich_vu AS dv ON hd.ma_dich_vu = dv.ma_dich_vu
-    --  GROUP BY kh.ma_khach_hang 
-     ORDER BY kh.ma_khach_hang;
 
--- Tong tiền cho mỗi mã hợp đồng
--- select  hdct.ma_hop_dong_chi_tiet, 
--- 		hdct.so_luong,
---         dvdk.gia,
--- 		hdct.so_luong * dvdk.gia  as tong
--- from hop_dong_chi_tiet  hdct
--- left join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
-
--- select kh.ma_khach_hang, hd.ma_hop_dong from khach_hang kh left join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
--- select*from dich_vu
--- select*from hop_dong
--- (select hd.ma_hop_dong, dv.chi_phi_thue + sum(hdct.so_luong * dvdk.gia) as tong_tien from hop_dong as hd
--- join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
--- left join hop_dong_chi_tiet as hdct on hd.ma_hop_dong = hdct.ma_hop_dong
--- join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
--- group by hd.ma_hop_dong)
--- WITH bang_tam(ma_hop_dong,
--- ma_hop_dong_chi_tiet,
--- so_luong,
--- gia,
--- tong) AS (
--- SELECT
--- 	hdct.ma_hop_dong AS ma_hop_dong,
--- 	hdct.ma_hop_dong_chi_tiet AS ma_hop_dong_chi_tiet,
--- 	hdct.so_luong AS so_luong,
--- 	dvdk.gia AS gia,
--- 	hdct.so_luong * dvdk.gia AS tong
--- FROM
--- 	hop_dong_chi_tiet hdct
--- LEFT JOIN dich_vu_di_kem dvdk ON
--- 	hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem)  
--- SELECT
--- 	hd.ma_hop_dong,
---     hd.ma_khach_hang,
---     hd.ma_dich_vu,
---     bang_tam.ma_hop_dong_chi_tiet,
---     bang_tam.so_luong,
---     bang_tam.gia,
---     bang_tam.tong,
---     kh.ho_ten,
---     hd.ngay_lam_hop_dong
-
--- FROM
--- 	bang_tam,
--- 	hop_dong hd,
---     dich_vu dv,
---     khach_hang kh
---     
--- WHERE
--- 	bang_tam.ma_hop_dong = hd.ma_hop_dong 
---     and hd.ma_khach_hang = kh.ma_khach_hang 
---     and hd.ma_dich_vu = dv.ma_dich_vu;
---     -- and kh.ma_loai_khach = lk.ma_loai_khach;
+-- Cach 2
+--      CREATE TEMPORARY TABLE bang_tam (SELECT    
+--        kh.ma_khach_hang, SUM(IFNULL(hdct.so_luong,0 ) * IFNULL(dvdk.gia,0)) AS tong_tien
+--       FROM khach_hang  kh
+--       LEFT JOIN hop_dong  hd ON kh.ma_khach_hang = hd.ma_khach_hang
+--       LEFT JOIN hop_dong_chi_tiet  hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+--       LEFT JOIN dich_vu_di_kem  dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+--       GROUP BY kh.ma_khach_hang);
      
+ -- Cach 1    
+	SELECT    
+      kh.ma_khach_hang, 
+      kh.ho_ten,
+      lk.ten_loai_khach, 
+      hd.ma_hop_dong, 
+      dv.ten_dich_vu,
+      hd.ngay_lam_hop_dong,
+      hd.ngay_ket_thuc,
+      dv.chi_phi_thue,
+      dv.chi_phi_thue + SUM(IFNULL(hdct.so_luong,0 ) * IFNULL(dvdk.gia,0))-- + bang_tam.tong_tien
+      FROM khach_hang AS kh
+	  JOIN loai_khach AS lk ON kh.ma_loai_khach = lk.ma_loai_khach
+      LEFT JOIN hop_dong AS hd ON kh.ma_khach_hang = hd.ma_khach_hang
+      LEFT JOIN dich_vu AS dv ON hd.ma_dich_vu = dv.ma_dich_vu
+	  -- LEFT JOIN bang_tam  ON bang_tam.ma_khach_hang = kh.ma_khach_hang
+      LEFT JOIN hop_dong_chi_tiet  hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+      LEFT JOIN dich_vu_di_kem  dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+      GROUP BY kh.ma_khach_hang ,hd.ma_hop_dong
+      ORDER BY kh.ma_khach_hang;
+     
+     -- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
+     -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+     SELECT 
+     dv.ma_dich_vu,
+     dv.ten_dich_vu,
+     dv.dien_tich,
+     dv.chi_phi_thue,
+     ldv.ten_loai_dich_vu,
+     hd.ngay_lam_hop_dong
+     FROM dich_vu dv
+     JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+     JOIN hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+     GROUP BY  dv.ma_dich_vu 
+     HAVING hd.ngay_lam_hop_dong <= DATE('2021-01-01')
+     OR hd.ngay_lam_hop_dong >= DATE('2021-03-31');
+     
+     -- 7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu 
+     -- của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+    SELECT DISTINCT 
+    dv.ma_dich_vu, 
+    dv.ten_dich_vu, 
+    dv.dien_tich, 
+    dv.so_nguoi_toi_da, 
+    dv.chi_phi_thue, 
+    ldv.ten_loai_dich_vu
+    FROM dich_vu dv
+    JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+    JOIN hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+    WHERE YEAR(hd.ngay_lam_hop_dong) = 2020 AND dv.ma_dich_vu
+		NOT IN (
+			SELECT dv.ma_dich_vu
+			FROM dich_vu dv
+			JOIN hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+			WHERE YEAR(hd.ngay_lam_hop_dong) = 2021
+        );
+	-- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+	-- Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+    -- Cach 1
+		SELECT kh.ho_ten 
+        FROM khach_hang kh
+        GROUP BY kh.ho_ten;
+	-- Cach 2
+		SELECT DISTINCT ho_ten 
+        FROM khach_hang ;
+	-- Cach 3
+ 		SELECT kh.ho_ten 
+        FROM khach_hang kh
+        union
+        SELECT kh.ho_ten 
+        FROM khach_hang kh;
+	
+    -- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+		SELECT MONTH (hd.ngay_lam_hop_dong),COUNT(hd.ma_khach_hang)
+        FROM hop_dong hd
+        WhERE YEAR(hd.ngay_lam_hop_dong)=2021
+        GROUP BY MONTH (hd.ngay_lam_hop_dong)
+        ORDER BY MONTH (hd.ngay_lam_hop_dong);
+ -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+ -- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).       
+        SELECT hd.ma_hop_dong, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, hd.tien_dat_coc, hdct.so_luong, SUM(hdct.so_luong)  AS so_lương_dich_vu_di_kem
+        FROM hop_dong hd
+        JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+        GROUP BY hd.ma_hop_dong;
+        
+        
 
  -- SET GLOBAL sql_mode='STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_ENGINE_SUBSTITUTION';
