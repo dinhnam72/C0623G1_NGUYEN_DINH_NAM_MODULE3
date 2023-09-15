962b9f648ac87bc6c80a91ba7519c0d21b7d493b
dinhnam72 <dinhnam20162011@gmail.com>
2023-09-15 17:19:35 +0700

    nop bai tap case case study

--- case_study/furama.sql
+++ case_study/furama.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE furama;
-use furama;
+USE furama;
 CREATE TABLE vi_tri(
 	ma_vi_tri INT PRIMARY KEY AUTO_INCREMENT,
     ten_vi_tri VARCHAR(45),
@@ -319,10 +319,10 @@ OR SUBString_INDEX(ho_ten,' ',-1) LIKE 'K%');
         FROM khach_hang kh;
 	
     -- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
-		SELECT MONTH (hd.ngay_lam_hop_dong),COUNT(hd.ma_khach_hang)
+		SELECT  MONTH (hd.ngay_lam_hop_dong) AS thang, COUNT(hd.ma_khach_hang) AS so_khach_hang_dat
         FROM hop_dong hd
         WhERE YEAR(hd.ngay_lam_hop_dong)=2021
-        GROUP BY MONTH (hd.ngay_lam_hop_dong)
+		GROUP BY MONTH (hd.ngay_lam_hop_dong)
         ORDER BY MONTH (hd.ngay_lam_hop_dong);
  -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
  -- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).       
@@ -330,7 +330,77 @@ OR SUBString_INDEX(ho_ten,' ',-1) LIKE 'K%');
         FROM hop_dong hd
         JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
         GROUP BY hd.ma_hop_dong;
+    
+-- 11.	Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.    
+       SELECT kh.ma_khach_hang, dvdk.ma_dich_vu_di_kem, dvdk.ten_dich_vu_di_kem, dvdk.gia, dvdk.don_vi , dvdk.trang_thai , lk.ten_loai_khach, kh.dia_chi
+       FROM khach_hang kh
+       JOIN loai_khach lk ON kh.ma_loai_khach= lk.ma_loai_khach
+       JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+       JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+       JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+       WHERE lk.ten_loai_khach = 'Diamond'
+       AND 
+       (kh.dia_chi like '%Vinh' OR kh.dia_chi like '%Quảng Ngãi');
+       
+ -- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), 
+ -- so_dien_thoai (khách hàng), ten_dich_vu, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), tien_dat_coc của 
+ -- tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.      
+       SELECT 
+	    hd.ma_hop_dong , 
+       nv.ho_ten AS ho_ten_nhan_vien, 
+       kh.ho_ten AS ho_ten_khach_hang, 
+       kh.so_dien_thoai,
+       dv.ten_dich_vu,
+	   SUM(hdct.so_luong) AS so_luong_dich_vu_di_kem,
+       hd.tien_dat_coc
+       FROM khach_hang kh 
+	    JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+	    JOIN dich_vu dv  ON dv.ma_dich_vu = hd.ma_dich_vu
+	    LEFT JOIN nhan_vien nv ON hd.ma_nhan_vien = nv.ma_nhan_vien
+	    LEFT JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+       WHERE YEAR( hd.ngay_lam_hop_dong)=2020 
+				AND QUARTER( hd.ngay_lam_hop_dong) = 4
+				AND hd.ma_dich_vu 
+                NOT IN ( SELECT hd.ma_dich_vu
+						FROM hop_dong hd
+						WHERE hd.ngay_lam_hop_dong >='2021-01-01' AND hd.ngay_lam_hop_dong <='2021-06-30')
+       GROUP BY hd.ma_hop_dong;
+       
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+-- (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).   
+		CREATE VIEW dich_vu_di_kem_nhieu_nhat AS
+		SELECT 
+        dvdk.ma_dich_vu_di_kem,
+        dvdk.ten_dich_vu_di_kem,
+	    (SUM(hdct.so_luong)) AS tong_dich_vu_di_kem
+        FROM khach_hang kh
+        JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+		JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+		JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+		GROUP BY dvdk.ma_dich_vu_di_kem;
+      
+        SELECT *
+        FROM dich_vu_di_kem_nhieu_nhat
+        WHERE tong_dich_vu_di_kem= 
+        (SELECT MAX(tong_dich_vu_di_kem)
+        FROM dich_vu_di_kem_nhieu_nhat);
         
-        
+ -- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. 
+ -- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).       
+        SELECT 
+        DISTINCT
+        hd.ma_hop_dong,
+        ldv.ten_loai_dich_vu,
+        dvdk.ten_dich_vu_di_kem,
+	    (COUNT(hdct.so_luong)) AS so_lan_dich_vu_di_kem
+        FROM khach_hang kh
+        JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+        JOIN dich_vu dv ON dv.ma_dich_vu = hd.ma_dich_vu
+        JOIN loai_dich_vu ldv ON ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+		JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+		JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+		GROUP BY dvdk.ma_dich_vu_di_kem
+        HAVING COUNT(hdct.so_luong) = 1;
+      
 
  -- SET GLOBAL sql_mode='STRICT_TRANS_TABLES,STRICT_ALL_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,TRADITIONAL,NO_ENGINE_SUBSTITUTION';
